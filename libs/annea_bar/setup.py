caebd4caa1edabbdc31c79b1f7b61eea7b7c9d07
horken7 <horken7@gmail.com>
2021-12-21 15:36:06 +0100

    include more info in setup file

--- libs/annea_bar/setup.py
+++ libs/annea_bar/setup.py
@@ -1,12 +1,19 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='annea_bar',
-    version="0.1.1",
+    version="0.1.2",
     description="An example python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="horken7",
-    author_email="johan.horken@annea.ai",
+    author_email="Johan Larsson Hörkén <johan.horken@annea.ai>",
     license="MIT",
     packages=['src'],
     install_requires=[],
+    url="https://github.com/annea-ai/python-framework/tree/master/libs/annea_bar"
 )
--- libs/annea_foo/setup.py
+++ libs/annea_foo/setup.py
@@ -1,15 +1,22 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='annea_foo',
-    version="0.1.1",
-    author="horken7",
-    author_email="johan.horken@annea.ai",
+    version="0.1.2",
     description="An example python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="horken7",
+    author_email="Johan Larsson Hörkén <johan.horken@annea.ai>",
     license="MIT",
     packages=['src'],
     install_requires=[
         'annea_bar',
         # add more packages if needed
     ],
+    url="https://github.com/annea-ai/python-framework/tree/master/libs/annea_foo"
 )

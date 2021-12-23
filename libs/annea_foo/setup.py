ae9d5d62ce096f6ee2a1ee4e7f21e938b1ad1932
horken7 <horken7@gmail.com>
2021-12-23 15:29:56 +0100

    add default version for local development
    - makes it possible to build and test the libs locally without the circle build num

--- libs/annea_foo/setup.py
+++ libs/annea_foo/setup.py
@@ -6,9 +6,14 @@ from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+if 'CIRCLE_BUILD_NUM' in os.environ:
+    version = f"0.1.{os.getenv('CIRCLE_BUILD_NUM')}"
+else:
+    version = '0.1.0'
+
 setup(
     name='annea_foo',
-    version=f"0.1.{os.getenv('CIRCLE_BUILD_NUM')}",
+    version=version,
     description="An example python package",
     long_description=long_description,
     long_description_content_type="text/markdown",
